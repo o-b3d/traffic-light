a8364209b70f1a498f412ebdb3dd78fe2dab8b57
o-b3d <obed@ogtesting.com>
2023-11-09 20:59:36 -0500

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,76 @@ import "./App.css";
 import { Light } from "./Light.jsx";
 import { useEffect, useState } from "react";
 
+{
+  /*The below line is where component begins*/
+}
 export default function App() {
-  const [activeLight, setActiveLight] = useState("red");
+  const [trafficLight, setTrafficLight] = useState("red");
 
   useEffect(() => {
+    {
+      /*if is entered here to detect if state is set to false using trafficLightStopped
+    and stop the function that has "switch" from running if trafficLightStopped is set to true
+    using the "return" at the end of the if statement*/
+    }
+    if (trafficLightStopped) return;
+
     const interval = setInterval(() => {
-      switch (activeLight) {
+      switch (trafficLight) {
         case "red":
-          setActiveLight("yellow");
+          setTrafficLight("yellow");
           break;
         case "yellow":
-          setActiveLight("green");
+          setTrafficLight("green");
           break;
         case "green":
-          setActiveLight("red");
+          setTrafficLight("red");
       }
     }, 1000);
     return () => clearInterval(interval);
   });
 
+  const switchTrafficLight = () => {
+    switch (trafficLight) {
+      case "red":
+        setTrafficLight("yellow");
+        break;
+      case "yellow":
+        setTrafficLight("green");
+        break;
+      case "green":
+        setTrafficLight("red");
+    }
+  };
+
+  {
+    /*trafficLightStopped is a variable used to change State of component to false to stop*/
+  }
+  const [trafficLightStopped, setTrafficLightStopped] = useState(false);
+
+  const handleStopButton = () => {
+    //prettier-ignore
+    {/*setTrafficLightStopped is used in a function to call "set Value" of variable above.
+    toggleState is parameter used in setTrafficLightStopped to change between true to false
+    in State using the ! symbol*/}
+    setTrafficLightStopped((toggleState) => !toggleState);
+  };
+
+
   return (
     <>
       <div className="trafficPole" />
-      <div className="trafficLight">
-        <Light color="red" opacity={activeLight === "red" ? 1 : 0.4} />
-        <Light color="yellow" opacity={activeLight === "yellow" ? 1 : 0.4} />
-        <Light color="green" opacity={activeLight === "green" ? 1 : 0.4} />
+      <div className="trafficLights">
+        <Light color="red" opacity={trafficLight === "red" ? 1 : 0.4} />
+        <Light color="yellow" opacity={trafficLight === "yellow" ? 1 : 0.4} />
+        <Light color="green" opacity={trafficLight === "green" ? 1 : 0.4} />
       </div>
-      <h1 style={{ margin: "auto", width: "fit-content" }}>{activeLight}</h1>
+      <button onClick={switchTrafficLight}>Switch ↻</button>
+      <button onClick={handleStopButton}>
+        {/*if statement using ? operator to use trafficLightStopped as condition to
+        detect if state is true or false and show corresponding text depending on state*/}
+        {trafficLightStopped ? "Start 👍" : "Stop ✋"}
+      </button>
     </>
   );
 }
